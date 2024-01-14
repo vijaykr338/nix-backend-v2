b917e88186a6e2cca10ec031c39c9f37ebc762a8
dvishal485 <dvishal485@gmail.com>
2024-01-14 13:03:42 +0530

    Refactor logsMiddleware to allow custom number of lines
    Signed-off-by: dvishal485 <dvishal485@gmail.com>

--- src/api/middlewares/logsMiddleware.ts
+++ src/api/middlewares/logsMiddleware.ts
@@ -3,9 +3,11 @@ import { exec as exec_sync } from "node:child_process";
 const exec = util.promisify(exec_sync);
 import asyncErrorHandler from "../helpers/asyncErrorHandler";
 
-const num_lines = 150;
+const default_num_lines = 150;
 
 export const get_logs = asyncErrorHandler(async (req, res, _next) => {
+  const num_lines = req.body.lines || default_num_lines;
+
   const { stdout } = await exec(`npx pm2 logs --nostream NixBackend --lines ${num_lines} --raw --out`);
   const { stdout: stderr } = await exec(`npx pm2 logs --nostream NixBackend --lines ${num_lines} --raw --err`);
   res.send(`<h1>stdout</h1><pre>${stdout}</pre><h1>stderr</h1><pre>${stderr}</pre>`);
