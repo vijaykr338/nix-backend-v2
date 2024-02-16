1ef7c6421c1f849c293322440ba78a2e4b27ce2f
Vishal <dvishal485@gmail.com>
2024-02-16 13:10:47 +0530

    added avatar upload and delete
    Signed-off-by: Vishal <dvishal485@gmail.com>

---
+++ src/api/middlewares/imageMiddleware.ts
@@ -0,0 +1,65 @@
+import multer from "multer";
+import CustomError from "../../config/CustomError";
+import StatusCode from "../helpers/httpStatusCode";
+
+
+export const storage = multer({
+  fileFilter: function (req, file, callback) {
+    const fileExtension = (file.originalname.split(".")[file.originalname.split(".").length - 1]).toLowerCase();
+
+    if (["png", "jpg", "jpeg"].indexOf(fileExtension) === -1) {
+      return callback(null, false);
+    }
+    callback(null, true);
+  },
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+      const updation_req = req.path.includes("update");
+      const filename_original = file.originalname.replace(/[^a-z0-9\s.]/gi, "").replace(/\s/g, "_");
+      if (updation_req) {
+        const file_name = req.params.filename;
+        if (!file_name) {
+          // todo: fix throw if uncaught by our custom error => should not occur because of the way we route here
+          throw "filename of replaced file not supplied";
+        } else {
+          cb(null, req.params.filename);
+        }
+      } else {
+        cb(null, `${Date.now().toString()}-${filename_original}`);
+      }
+    },
+  })
+});
+
+
+export const avatarStorage = multer({
+  fileFilter: function (req, file, callback) {
+    const fileExtension = (file.originalname.split(".")[file.originalname.split(".").length - 1]).toLowerCase();
+
+    if (["png", "jpg", "jpeg"].indexOf(fileExtension) === -1) {
+      return callback(null, false);
+    }
+    callback(null, true);
+  },
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+      const filename = req.body.user_id;
+      req.query.thumbnail = "true"; // to force generate thumbnail in next middleware
+      if (!filename) {
+        // should not be rechable because of the way we route here through protect middleware
+        const err = new CustomError("Required information not found! Please relogin.", StatusCode.BAD_REQUEST);
+        return cb(err, "error");
+      }
+      cb(null, filename);
+    },
+  }),
+  limits: {
+    fileSize: 1024 * 1024 * 5, // 5MB
+  }
+});
--- src/api/routes/imageRouter.ts
+++ src/api/routes/imageRouter.ts
@@ -1,40 +1,10 @@
 import express from "express";
-import protected_route from "../middlewares/permsMiddlewareInit";
+import { delete_avatar, delete_image, get_image, update_image, upload_image } from "../controllers/imageController";
 import Permission from "../helpers/permissions";
 import { protect } from "../middlewares/authMiddleware";
-import multer from "multer";
-import { delete_image, get_image, update_image, upload_image } from "../controllers/imageController";
+import { avatarStorage, storage } from "../middlewares/imageMiddleware";
+import protected_route from "../middlewares/permsMiddlewareInit";
 
-const storage = multer({
-  fileFilter: function (req, file, callback) {
-    const fileExtension = (file.originalname.split(".")[file.originalname.split(".").length - 1]).toLowerCase();
-
-    if (["png", "jpg", "jpeg"].indexOf(fileExtension) === -1) {
-      return callback(null, false);
-    }
-    callback(null, true);
-  },
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "uploads/");
-    },
-    filename: (req, file, cb) => {
-      const updation_req = req.path.includes("update");
-      const filename_original = file.originalname.replace(/[^a-z0-9\s.]/gi, "").replace(/\s/g, "_");
-      if (updation_req) {
-        const file_name = req.params.filename;
-        if (!file_name) {
-          // todo: fix throw if uncaught by our custom error => should not occur because of the way we route here
-          throw "filename of replaced file not supplied";
-        } else {
-          cb(null, req.params.filename);
-        }
-      } else {
-        cb(null, `${Date.now().toString()}-${filename_original}`);
-      }
-    },
-  })
-});
 
 const router = express.Router();
 
@@ -42,8 +12,10 @@ const protect_upload = protected_route([Permission.UploadImage]);
 const protect_update = protected_route([Permission.UpdateImage]);
 const protect_delete = protected_route([Permission.DeleteImage]);
 
-router.route("/update/:filename").put(protect, protect_update, storage.single("avatar"), update_image);
-router.route("/upload").post(protect, protect_upload, storage.single("avatar"), upload_image);
+router.route("/update/:filename").put(protect, protect_update, storage.single("image"), update_image);
+router.route("/upload").post(protect, protect_upload, storage.single("image"), upload_image);
+router.route("/upload-avatar").post(protect, avatarStorage.single("avatar"), upload_image);
+router.route("/delete-avatar").delete(protect, delete_avatar);
 router.route("/get/:filename").get(get_image);
 router.route("/delete/:filename").delete(protect, protect_delete, delete_image);
 
