c1cd5ebfcf04b8ca2f07ebb7173ff0e077f76310
pranjal.trvd <pranjaltrvd12@gmail.com>
2024-03-29 15:00:34 +0530

    update profile added

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -4,6 +4,7 @@ import CustomError from "../../config/CustomError";
 import mongoose from "mongoose";
 import Permission from "../helpers/permissions";
 import StatusCode from "../helpers/httpStatusCode";
+import bcrypt from "bcrypt";
 
 export const getAllUsers = asyncErrorHandler(async (req, res) => {
   //add logic here
@@ -70,3 +71,86 @@ export const getCurrentUserController = asyncErrorHandler(
     });
   },
 );
+
+
+/**
+ * Updates the user's profile.
+ * Users can update their bio, password, and username.
+ * If the user has the permission to Update Profile, they can also update Role, extra_perms, and removed_perms.
+ */
+export const updateUserController = asyncErrorHandler(async (req, res, next) => {
+  const user_id = new mongoose.Types.ObjectId(req.body.user_id);
+  // Check if the user exists
+  const user = await UserService.checkUserExists({ _id: user_id });
+
+  if (!user) {
+    const error = new CustomError(
+      "Unable to get current user",
+      StatusCode.UNAUTHORIZED,
+    );
+    return next(error);
+  }
+
+  // Update user properties if provided in request body
+  if (req.body.name) user.name = req.body.name;
+  if (req.body.password) {
+    const hashed_password: string = await bcrypt.hash(req.body.password, 10);
+    user.password = hashed_password;
+  }
+  if (req.body.bio) user.bio = req.body.bio;
+
+  // If user has permission to update profile
+  if (req.body.extra_permissions || req.body.removed_permissions || req.body.role_id) {
+    const allowed_perms: Set<Permission> = new Set();
+    user.extra_permissions?.forEach((perm) => allowed_perms.add(perm));
+    user?.role_id?.permissions?.forEach((perm) => allowed_perms.add(perm));
+    user.removed_permissions?.forEach((perm) => allowed_perms.delete(perm));
+
+    const permissions = [...allowed_perms];
+
+    if (permissions.includes(Permission.UpdateProfile)) {
+      if (req.body.role_id) user.role_id = req.body.role_id;
+      if (req.body.extra_permissions) user.extra_permissions = req.body.extra_permissions;
+      if (req.body.removed_permissions) user.removed_permissions = req.body.removed_permissions;
+    }
+  }
+
+  // Save the updated user
+  await user.save();
+
+  res.status(StatusCode.OK).json({
+    status: "success",
+    message: "Profile Updated",
+    data: { user }
+  });
+});
+
+
+/**
+ * Allows a user with permission to update profiles to update Role and Perms of other members.
+ */
+export const permsUpdateController = asyncErrorHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const user_id = new mongoose.Types.ObjectId(id);
+  const user = await UserService.checkUserExists({ _id: user_id });
+
+  if (!user) {
+    const error = new CustomError(
+      "Requested user not found",
+      StatusCode.NOT_FOUND,
+    );
+    return next(error);
+  }
+
+  if (req.body.role_id) user.role_id = req.body.role_id;
+  if (req.body.extra_permissions) user.extra_permissions = req.body.extra_permissions;
+  if (req.body.removed_permissions) user.removed_permissions = req.body.removed_permissions;
+
+  await user.save();
+
+  res.status(StatusCode.OK).json({
+    status: "success",
+    message: "Perms Updated",
+    data: { user }
+  })
+});
--- src/api/routes/userRoute.ts
+++ src/api/routes/userRoute.ts
@@ -2,12 +2,23 @@ import express from "express";
 import {
   getAllUsers,
   getCurrentUserController,
+  permsUpdateController,
+  updateUserController,
 } from "../controllers/userController";
 import { protect } from "../middlewares/authMiddleware";
+import protected_route from "../middlewares/permsMiddlewareInit";
+import Permission from "../helpers/permissions";
+
 const router = express.Router();
+
+const UpdateProfileProtect = protected_route([Permission.UpdateProfile]);
 
 router.route("/").get(protect, getAllUsers);
 
 router.route("/current-user").get(protect, getCurrentUserController);
+
+router.route("/update-user").put(protect, updateUserController);
+
+router.route("/update-perms/:id").put(protect, UpdateProfileProtect, permsUpdateController);
 
 export default router;
